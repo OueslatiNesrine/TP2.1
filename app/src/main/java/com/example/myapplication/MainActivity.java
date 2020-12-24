e59aec255593dd530a141a8edb0c6d5eba873651
Hamza zarred <hamzazarred9595@gmail.com>
2020-12-24 21:16:40 +0100

    app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    public static final String EXTRA_MESSAGE = "com.example.android.Hellotoast.extra.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
+    }
+
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this , MainActivity2.class);
+        intent.putExtra(EXTRA_MESSAGE,mShowCount.getText().toString());
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String nombre = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.count_view);
+        textView.setText(nombre);
+    }
+}
